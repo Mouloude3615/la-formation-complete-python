3d297892ec7a867141c82e982236da439256017d
Mouloude3615 <56880445+Mouloude3615@users.noreply.github.com>
2022-08-08 15:15:24 +0100

    Update recuperer_input.py
    nom = input("Quel est votre nom: ")
    ville = input(" Quelle est votre ville :")
    age = input("quel âge avez vous? ")
    print(nom)
    print(ville)
    print(age)

--- exr-001_input-utilisateur/01-sources/recuperer_input.py
+++ exr-001_input-utilisateur/01-sources/recuperer_input.py
@@ -1,2 +1,9 @@
 # Demander le prénom, la ville de naissance et l'âge de la personne qui utilise le script
 # Afficher ces trois informations
+nom = input("Quel est votre nom: ")
+ville = input(" Quelle est votre ville :")
+age = input("quel âge avez vous? ")
+print(nom)
+print(ville)
+print(age)
+      
